46338171a40d8d90fd489fa5985976a8f62c1249
EvertonemDia <everton.silva@euemdia.com.br>
2020-11-27 11:42:55 -0300

    AJustes

---
+++ Automation/BDD/TesteAPI.feature
@@ -0,0 +1,16 @@
+﻿#language:pt-BR
+
+Funcionalidade:1 TesteAPI
+Contexto: 
+
+
+Esquema do Cenário: Api weather
+	Quando faco a requisicao usando a <city>
+	Entao o status code deve ser <status>
+	
+
+	Exemplos: 
+	| city          | status |
+	| Round Rock,TX | 200    |
+	| Tampa,TX      | 204    |
+	|               | 400    |
---
+++ Automation/Steps/TesteApiSteps.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Automation.Steps
+{
+    [Binding]
+    class TesteApiSteps 
+    {
+        int status;
+
+        [When(@"faco a requisicao usando a (.*)")]
+        public void QuandoFacoARequisicaoUsandoA(string city)
+        {
+            var client = new RestClient($"https://api.weatherbit.io/v2.0/current?city={city}&key=6b7249db33e048d69bf71a79d5e6e95e");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            status = (int) response.StatusCode;
+        }
+
+        [Then(@"o status code deve ser (.*)")]
+        public void EntaoOStatusCodeDeveSer(int status)
+        {
+            Assert.AreEqual(this.status, status);
+        }
+    }
+}
